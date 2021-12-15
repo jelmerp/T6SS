e91327fc474eae54b94cbf3d78218488ab88e990
jelmerp <jelmerpoelstra@gmail.com>
2021-12-15 09:07:04 -0500

    Add code to download all FNAs for a COG

--- workflow/workflow.sh
+++ workflow/workflow.sh
@@ -3,46 +3,56 @@ dir_refseq=results/refseq
 
 conda activate blast-env
 
-## Get protein sequences for a gene
-esearch -db protein -query "vgrG AND \"Pseudomonas putida\"[Organism]" | 
-    efetch -format fasta > "$dir_refseq"/vgrG_Pputida_protein.fa
-#esearch -db protein -query "vgrG AND \"Pseudomonas putida\"[Organism]"  # 652
-#esearch -db protein -query "vgrG AND txid303[Organism:exp]"             # 652 = P. putida
-#esearch -db protein -query "vgrG AND txid286[Organism:exp]"             # 54137 = genus Pseudomonas
-#TODO - select only complete proteins
+## Download EggNOG list
+base_URL=http://eggnogapi5.embl.de/nog_data/file/extended_members
 
-esearch -db gene -query "vgrG AND \"Pseudomonas putida\"[Organism]" |
-    esummary |
-    xtract -pattern DocumentSummary -element Id
+#TODO - Get list of COGS
 
-## Get nucleotide FASTA from protein ID
-esearch -query "ABY99126.1" -db gene |
-    esummary |
-    xtract -pattern GenomicInfoType -element ChrAccVer -1-based ChrStart ChrStop |
-    xargs -n 3 sh -c 'efetch -db nuccore -id "$0" -seq_start "$1" -seq_stop "$2" -format fasta'
-esearch -query "ABY99126.1" -db gene | efetch -format documentsummary
-# esearch -query 108765232 -db protein | elink -target gene # alt
+cog_id="COG3501"
 
-esearch -query "WP_010954844.1" -db gene |
-    esummary
-
-esearch -query "PP4_24660" -db gene | esummary
+cog_tsv="$dir_refseq"/"$cog_id".tsv.gz
+cog_fa="$dir_refseq"/"$cog_id".fa
+loc_info="$dir_refseq"/"$cog_id"_locs.tsv
 
 ## Download EggNOG list
-fCOG="COG3501"
-wget http://eggnogapi5.embl.de/nog_data/file/extended_members/"$fCOG" -O "$fCOG.tsv.gz"
-accessions=( $(zcat $fCOG.tsv.gz | grep "Pseudomonas putida" | cut -f 2) )
+wget "$base_URL"/"$cog_id" -O "$cog_tsv" 
 
+## Save columns with accession nr's, species names, and taxids
+mapfile ACCESSIONS -t array < <(zcat "$cog_tsv" | cut -f 2)
+mapfile SPECIES -t array < <(zcat "$cog_tsv" | cut -f 3)
+mapfile TAXIDS -t array < <(zcat "$cog_tsv" | cut -f 4)
 
-##
-esearch -db gene -query "vgrG AND \"Pseudomonas putida\"[Organism] AND ALIVE[PROP]" |
-    esummary |
-    xtract -pattern GenomicInfoType -element ChrAccVer -1-based ChrStart ChrStop |
-    xargs -n 3 sh -c 'efetch -db nuccore -id "$0" -seq_start "$1" -seq_stop "$2" -format fasta' \
-    > "$dir_refseq"/vgrG_Pputida_gene.fa
+## Get genomic location for each accession
+for i in "${!ACCESSIONS[@]}"; do
+    acc="${ACCESSIONS[$i]}" #txid=1211579; acc=PP4_27770
+    spec="${SPECIES[$i]}"
+    txid="${TAXIDS[$i]}"
+    echo "## i: $i" >&2
+    printf "## Accession: %s" "$acc" >&2
+    printf "## Species: %s" "$spec" >&2
+    printf "%s\t%s\t%s\t" "${acc%$'\n'}" "${spec%$'\n'}" "${txid%$'\n'}"
 
-esearch -db gene -query "vgrG AND \"Pseudomonas putida\"[Organism] AND ALIVE[PROP]" |
-    esummary |
-    xtract -pattern DocumentSummary -element Id
+    docsum=$(esearch -query "$acc AND txid${txid}[Organism:exp]" -db gene | efetch -format docsum)
+    docsum=$(echo $docsum | sed -s 's@</DocumentSummary>.*@</DocumentSummary>@') # Only take first record
 
+    if [ -n "$docsum" ]; then
+        printf "## Hit found for: " >&2
+        echo $docsum | xtract -pattern Organism -element ScientificName >&2
+        echo $docsum | xtract -pattern GenomicInfoType -element ChrAccVer -1-based ChrStart ChrStop
+    else
+        echo "## No hit found" >&2
+        printf "NA\tNA\tNA"
+    fi
+    printf "\n"
+    echo >&2
+done >"$loc_info".tmp
 
+## Get rid of empty lines and sort by taxid, seqid, and start position
+sed -e '/^$/d' $loc_info.tmp | sort -k3,3n -k4,4 -k5,5n > $loc_info
+
+## Get FASTA files
+while IFS=$'\t' read -r acc species txid seqid start stop; do
+    echo "## Species: $species / seqid: $seqid / start: $start / stop: $stop" >&2
+    efetch -db nuccore -id "$seqid" -seq_start "$start" -seq_stop "$stop" -format fasta |
+        sed -E "s/(>.*)$/\1 -- accession:$acc taxid:$txid/"
+done < <(grep -v "NA$" "$loc_info") >"$cog_fa"
