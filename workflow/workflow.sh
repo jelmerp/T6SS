dec859d838a0beb56525d6248ef218d9d1869133
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-01-20 16:18:20 -0500

    Get FASTAs with while loop

--- workflow/workflow.sh
+++ workflow/workflow.sh
@@ -1,58 +1,41 @@
-## Dirs and settings
-dir_refseq=results/refseq
-
 conda activate blast-env
+export NCBI_API_KEY=34618c91021ccd7f17429b650a087b585f08 # See https://support.nlm.nih.gov/knowledgebase/article/KA-05317/en-us, https://www.ncbi.nlm.nih.gov/books/NBK179288/
+
+## Specify COG
+#TODO - Get list of COGS
+cog_id="COG3501"
+
+## Set up output files
+dir_refseq=results/refseq
+cog_tsv="$dir_refseq"/"$cog_id".tsv.gz
+cog_fa="$dir_refseq"/"$cog_id".fa
+cog_acc="$dir_refseq"/"$cog_id"_accessions.tsv
 
 ## Download EggNOG list
 base_URL=http://eggnogapi5.embl.de/nog_data/file/extended_members
-
-#TODO - Get list of COGS
-
-cog_id="COG3501"
-
-cog_tsv="$dir_refseq"/"$cog_id".tsv.gz
-cog_fa="$dir_refseq"/"$cog_id".fa
-loc_info="$dir_refseq"/"$cog_id"_locs.tsv
-
-## Download EggNOG list
 wget "$base_URL"/"$cog_id" -O "$cog_tsv" 
 
-## Save columns with accession nr's, species names, and taxids
-mapfile ACCESSIONS -t array < <(zcat "$cog_tsv" | cut -f 2)
-mapfile SPECIES -t array < <(zcat "$cog_tsv" | cut -f 3)
-mapfile TAXIDS -t array < <(zcat "$cog_tsv" | cut -f 4)
-
-## Get genomic location for each accession
-for i in "${!ACCESSIONS[@]}"; do
-    acc="${ACCESSIONS[$i]}" #txid=1211579; acc=PP4_27770
-    spec="${SPECIES[$i]}"
-    txid="${TAXIDS[$i]}"
-    echo "## i: $i" >&2
-    printf "## Accession: %s" "$acc" >&2
-    printf "## Species: %s" "$spec" >&2
-    printf "%s\t%s\t%s\t" "${acc%$'\n'}" "${spec%$'\n'}" "${txid%$'\n'}"
-
-    docsum=$(esearch -query "$acc AND txid${txid}[Organism:exp]" -db gene | efetch -format docsum)
-    docsum=$(echo $docsum | sed -s 's@</DocumentSummary>.*@</DocumentSummary>@') # Only take first record
-
-    if [ -n "$docsum" ]; then
-        printf "## Hit found for: " >&2
-        echo $docsum | xtract -pattern Organism -element ScientificName >&2
-        echo $docsum | xtract -pattern GenomicInfoType -element ChrAccVer -1-based ChrStart ChrStop
-    else
-        echo "## No hit found" >&2
-        printf "NA\tNA\tNA"
+## Download FASTA
+printf "%s\t%s\t%s\t%s\n" "fa_id"  "accession" "species" "txid" >"$cog_acc"  # Print header for file
+while IFS=$'\t' read -ru9 _ acc spec txid _; do
+    ## Search for the focal accession and store the nr of hits in the database
+    res=$(esearch -query "$acc AND txid${txid}[Organism:exp]" -db protein)
+    nhits=$(echo $res | xtract -pattern ENTREZ_DIRECT -element Count)
+    
+    ## If there are 1 or more hits, print the FASTA to stdout and accession, species, and taxid to stderr
+    if [ "$nhits" -gt 0 ]; then
+        fa="$(echo $res | efetch -format fasta_cds_na | sed -E "s/(>.*)$/\1 [accession=$acc] [taxid=$txid]/")"
+        fa_id=$(echo "$fa" | grep ">" | sed -E 's/>([^ ]+) .*/\1/')  # Save FASTA ID
+        echo "$fa"
+        printf "%s\t%s\t%s\t%s\n" "$fa_id" "$acc" "$spec" "$txid" >&2
     fi
-    printf "\n"
-    echo >&2
-done >"$loc_info".tmp
+done 9< <(zcat "$cog_tsv" | grep "putida") >"$cog_fa" 2> >(tee -a "$cog_acc" >&2)
+#TODO - all taxa instead of putida only
 
-## Get rid of empty lines and sort by taxid, seqid, and start position
-sed -e '/^$/d' $loc_info.tmp | sort -k3,3n -k4,4 -k5,5n > $loc_info
+## Align and build a tree
+cog_aln=results/trees/"$cog_id".fa
+cog_tree=results/trees/"$cog_id".tre
+sbatch mcic-scripts/trees/tree-build.sh -i "$cog_fa" -o "$cog_aln" -t "$cog_tree" -l
 
-## Get FASTA files
-while IFS=$'\t' read -r acc species txid seqid start stop; do
-    echo "## Species: $species / seqid: $seqid / start: $start / stop: $stop" >&2
-    efetch -db nuccore -id "$seqid" -seq_start "$start" -seq_stop "$stop" -format fasta |
-        sed -E "s/(>.*)$/\1 -- accession:$acc taxid:$txid/"
-done < <(grep -v "NA$" "$loc_info") >"$cog_fa"
+fig=results/trees/COG3501.png
+Rscript mcic-scripts/trees/tree-plot.R -t "$cog_tree" -a "$cog_aln" -o "$fig" -n "$cog_acc" -c species 
